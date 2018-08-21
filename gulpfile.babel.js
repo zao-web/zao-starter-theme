457cdef63646e437309a71317e0382b05949cfb8
Liz Karaffa <liz@zao.is>
2018-08-21 08:33:19 -0700

    watch scss files not css files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,7 +11,7 @@ gulp.task( 'cssprocess', gulp.series( 'css', 'cssnano', 'cssclean' ) );
 gulp.task( 'watch', () => {
 	process.env.NODE_ENV = 'development';
 	livereload.listen( { basePath: 'dist' } );
-	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'cssprocess' ) );
+	gulp.watch( './assets/scss/**/*.scss', gulp.series( 'cssprocess' ) );
 	gulp.watch( './assets/js/**/*.js', gulp.series( 'js' ) );
 } );
 
