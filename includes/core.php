b26a4d0a9236f87fb8adc79c3ed65150452a590c
Liz Karaffa <liz@zao.is>
2018-08-21 15:56:50 -0700

    add theme support for customizer logo

--- includes/core.php
+++ includes/core.php
@@ -41,6 +41,7 @@ function i18n() {
  */
 function theme_setup() {
 	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'custom-logo' );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support(
