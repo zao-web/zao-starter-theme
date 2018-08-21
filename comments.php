2ba3c3c297565200479e82a7e0233731ccbf19cb
Liz Karaffa <liz@zao.is>
2018-08-21 16:23:01 -0700

    add the comments template

---
+++ comments.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage ZaoStarterTheme
+ * @since 1.0
+ * @version 2.0
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<section class="c-article-comments">
+
+	<?php
+	// You can start editing here -- including this comment!
+	if ( have_comments() ) :
+		?>
+		<h2 class="c-comments-title o-heading-3">
+			<?php
+				$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				/* translators: %s: post title */
+				printf( esc_html__( 'One Reply to &ldquo;%s&rdquo;', 'comments title' ), get_the_title() );
+			} else {
+				printf(
+					/* translators: 1: number of comments, 2: post title */
+					esc_html( _nx(
+						'%1$s Reply to &ldquo;%2$s&rdquo;',
+						'%1$s Replies to &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'zao-starter-theme'
+					) ),
+					esc_html( number_format_i18n( $comments_number ) ),
+					get_the_title()
+				);
+			}
+			?>
+		</h2>
+
+		<ol class="c-comment-list">
+			<?php
+				wp_list_comments(
+					array(
+						'avatar_size' => 100,
+						'style'       => 'ol',
+						'short_ping'  => true,
+						'reply_text'  => __( 'Reply', 'zao-starter-theme' ),
+					)
+				);
+			?>
+		</ol>
+
+		<?php
+	endif; // Check for have_comments().
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+		?>
+
+		<p class="c-no-comments"><?php esc_html_e( 'Comments are closed.', 'zao-starter-theme' ); ?></p>
+		<?php
+	endif;
+	comment_form();
+	?>
+
+</section>
