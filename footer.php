8300cb463a085ac96cb4617af924ecd9260f9265
Liz Karaffa <liz@zao.is>
2018-08-21 13:39:45 -0700

    add footer html

--- footer.php
+++ footer.php
@@ -1,11 +1,26 @@
 <?php
 /**
- * The template for displaying the footer.
+ * The footer for our theme
  *
- * @package ThemeScaffold
+ * This is the template that displays the ending of the page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage ZaoStarterTheme
+ * @since 1.0
+ * @version 2.0
  */
 
 ?>
-	<?php wp_footer(); ?>
-	</body>
+</main>
+<!-- site-content -->
+
+<!-- site-footer -->
+<?php get_template_part( 'template-parts/site-footer' ); ?>
+<!-- /site-footer -->
+
+</div> <!-- /site -->
+<?php wp_footer(); ?>
+</body>
 </html>
---
+++ template-parts/site-footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the site footer.
+ *
+ * @package WordPress
+ * @subpackage vanlig
+ * @since 1.0
+ * @version 2.0
+ */
+
+?>
+
+<footer class="o-site-footer" role="contentinfo">
+
+  <!-- main footer part -->
+  <div class="o-site-footer__main u-text-color-secondary">
+
+	<div class="l-wrap">
+
+	</div>
+
+  </div>
+  <!-- /main footer part -->
+
+</footer>
