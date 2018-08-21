a038f167b374227a82833f6ce10252b0ad0bd19c
Liz Karaffa <liz@zao.is>
2018-08-21 16:11:23 -0700

    create the single.php asset

---
+++ single.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage vanlig
+ * @since 1.0
+ * @version 2.0
+ */
+
+get_header();
+?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+  <div class="l-wrap">
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'c-news-content' ); ?>>
+	  <header class="c-news-content__header">
+		<?php the_title( '<h1 class="heading-1">', '</h1>' ); ?>
+		<h4 class="heading-4">Posted by: <?php the_author(); ?></h4>
+	  </header>
+	  <div class="c-news-content__article wysiwyg">
+		<?php the_content(); ?>
+	  </div>
+
+	  <!-- Comments -->
+	  <?php comments_template(); ?>
+	  <!-- Comments -->
+
+	  <!-- Sharing -->
+	  <?php get_template_part( 'template-parts/module-sharing' ); ?>
+	  <!-- /Sharing -->
+	</article>
+  </div>
+<?php endwhile; ?>
+
+<?php get_sidebar(); ?>
+<?php
+get_footer();
