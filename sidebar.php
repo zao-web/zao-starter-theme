2fe537d5795abbe488027c5ab513ee7fa5f362b6
Liz Karaffa <liz@zao.is>
2018-08-23 08:35:24 -0700

    create sidebar asset

---
+++ sidebar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The sidebar for our theme
+ *
+ * This is the template that acts as a sidebar for some pages.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage vanlig
+ * @since 1.0
+ * @version 2.0
+ */
+
+?>
+
+<aside class="c-sidebar" role="complementary">
+  <!-- widgets -->
+	<?php // dynamic_sidebar( 'sidebar-1' ); ?>
+  <!-- widgets -->
+</aside>
